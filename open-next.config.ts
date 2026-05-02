77d78e2f1c9488849ed4a2fbeed515817d7c286b
SH20RAJ <shaswatraj3@gmail.com>
2026-05-02 12:05:23 +0530

    fix: correct open-next config structure for minify

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,9 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
-	minify: true,
+	default: {
+		minify: true,
+	},
 	// Uncomment to enable R2 cache,
 	// It should be imported as:
 	// `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
